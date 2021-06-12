c22ae4e89037aa910d7af39fab6082bde23cf1ef
Ryan Lee <ryanjlee22@gmail.com>
2021-06-12 16:05:58 -0400

    Add problem explanation

--- src/dna.py
+++ src/dna.py
@@ -1,3 +1,6 @@
+#Counts the number of each specific nucleotides in a given DNA sequence.
+#Outputs in the order of adenine, cytosine, guanine, and thymine.
+
 sequence = input("Enter DNA sequence: ")
 
 a = 0;
