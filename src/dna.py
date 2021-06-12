12e4c9694d32245019ac1ec4b95ec6701f806ea4
Ryan Lee <ryanjlee22@gmail.com>
2021-06-12 15:55:52 -0400

    Add solution for dna problem

---
+++ src/dna.py
@@ -0,0 +1,19 @@
+sequence = input("Enter DNA sequence: ")
+
+a = 0;
+c = 0;
+g = 0;
+t = 0;
+
+
+for i in range(len(sequence)):
+	if sequence[i] == 'A':
+		a += 1;
+	if sequence[i] == 'C':
+		c += 1
+	if sequence[i] == 'G':
+		g += 1
+	if sequence[i] == 'T':
+		t += 1
+
+print(str(a) + " " + str(c) + " " + str(g) + " " + str(t))
